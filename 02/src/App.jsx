c884c26c5bf3c972fab5c4da81ec85b0f53fa206
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2026-03-28 16:14:37 -0700

    init

---
+++ 02/src/App.jsx
@@ -0,0 +1,23 @@
+const TURNOS ={
+  X: 'X',
+  O: 'O'
+}
+const board = Array(9).fill(null);
+
+function App() {
+  return (
+    <main className="board">
+      <h1>tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => (
+          <div className="cell" key={index}>
+            <span className="cell_content">
+              {index}
+            </span>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
+export default App;
